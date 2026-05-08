1872dc2a79c4f6949d6b3b3e0a3ea9644bd05ce4
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-08 20:58:31 -0300

    feat: adiciona classe Cliente extends Participante, só faltando métodos específicos para a classe

--- src/br/com/totvs/conversacional/entities/Cliente.java
+++ src/br/com/totvs/conversacional/entities/Cliente.java
@@ -1,4 +1,76 @@
 package br.com.totvs.conversacional.entities;
 
-public class Cliente {
+public class Cliente extends Participante {
+
+    private double budget;
+    private String tipoDecisao;
+    private String satisfacao;
+    private boolean mencionouConcorrente;
+
+    public Cliente(){}
+
+    public Cliente(String nome, String cargo, String empresa, double budget, String tipoDecisao, String satisfacao, boolean mencionouConcorrente) {
+        super(nome, cargo, empresa);
+        this.budget = budget;
+        this.tipoDecisao = tipoDecisao;
+        this.satisfacao = satisfacao;
+        this.mencionouConcorrente = mencionouConcorrente;
+    }
+
+    public Cliente(double budget, String tipoDecisao, String satisfacao, boolean mencionouConcorrente) {
+        this.budget = budget;
+        this.tipoDecisao = tipoDecisao;
+        this.satisfacao = satisfacao;
+        this.mencionouConcorrente = mencionouConcorrente;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public String getTipoDecisao() {
+        return tipoDecisao;
+    }
+
+    public void setTipoDecisao(String tipoDecisao) {
+        this.tipoDecisao = tipoDecisao;
+    }
+
+    public String getSatisfacao() {
+        return satisfacao;
+    }
+
+    public void setSatisfacao(String satisfacao) {
+        this.satisfacao = satisfacao;
+    }
+
+    public boolean isMencionouConcorrente() {
+        return mencionouConcorrente;
+    }
+
+    public void setMencionouConcorrente(boolean mencionouConcorrente) {
+        this.mencionouConcorrente = mencionouConcorrente;
+    }
+
+    @Override
+    public String apresentar() {
+        return "Cliente: " + nome + " | Empresa: " + empresa;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=== CLIENTE ===" +
+                "\nNome: " + nome +
+                "\nCargo: " + cargo +
+                "\nEmpresa: " + empresa +
+                "\nBudget: R$ " + budget +
+                "\nTipo de Decisao: " + tipoDecisao +
+                "\nSatisfacao: " + satisfacao +
+                "\nMencionou Concorrente: " + mencionouConcorrente;
+    }
 }
+
