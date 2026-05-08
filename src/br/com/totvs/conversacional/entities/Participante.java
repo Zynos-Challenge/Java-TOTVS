5c927d66cbaad07d39f74b294dfdaa8cb8ec297d
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-08 20:17:33 -0300

    feat: adiciona classe abstrata Participante com atributos e metodos base

--- src/br/com/totvs/conversacional/entities/Participante.java
+++ src/br/com/totvs/conversacional/entities/Participante.java
@@ -1,4 +1,51 @@
 package br.com.totvs.conversacional.entities;
 
-public class Participante {
+public abstract class Participante {
+
+    protected String nome;
+    protected String cargo;
+    protected String empresa;
+
+    public Participante() {
+    }
+
+    public Participante(String nome, String cargo, String empresa) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.empresa = empresa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    //metodo abstrato para cada filho implementar de um jeito
+    public abstract String apresentar();
+
+    @Override
+    public String toString() {
+        return  "\nNome: " + nome +
+                "\nCargo: " + cargo +
+                "\nEmpresa: " + empresa;
+    }
 }
