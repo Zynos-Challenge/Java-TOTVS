e83d61cbfb843bdd80abbb8fb1ac0af94f641912
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-12 18:45:35 -0300

    feat: adiciona classe Alerta

--- src/br/com/totvs/conversacional/entities/Alerta.java
+++ src/br/com/totvs/conversacional/entities/Alerta.java
@@ -1,4 +1,59 @@
 package br.com.totvs.conversacional.entities;
 
 public class Alerta {
+
+    private String tipo;
+    private String descricao;
+    private String urgencia;
+    private String trechoOrigem;
+
+    public Alerta(){}
+
+    public Alerta(String tipo, String descricao, String urgencia, String trechoOrigem) {
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.urgencia = urgencia;
+        this.trechoOrigem = trechoOrigem;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrgencia() {
+        return urgencia;
+    }
+
+    public void setUrgencia(String urgencia) {
+        this.urgencia = urgencia;
+    }
+
+    public String getTrechoOrigem() {
+        return trechoOrigem;
+    }
+
+    public void setTrechoOrigem(String trechoOrigem) {
+        this.trechoOrigem = trechoOrigem;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=== ALERTA ===" +
+                "\nTipo: " + tipo +
+                "\nDescricao: " + descricao +
+                "\nUrgencia: " + urgencia +
+                "\nTrecho de Origem: " + trechoOrigem;
+    }
 }
