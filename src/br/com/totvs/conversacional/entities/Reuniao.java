882f8eb7e3481266e9c8dccd6802a9f91c5a2d2d
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-12 18:54:14 -0300

    feat: adiciona classe Reuniao com LocalDateTime

--- src/br/com/totvs/conversacional/entities/Reuniao.java
+++ src/br/com/totvs/conversacional/entities/Reuniao.java
@@ -1,4 +1,61 @@
 package br.com.totvs.conversacional.entities;
 
+import java.time.LocalDateTime;
+
 public class Reuniao {
+
+    private LocalDateTime data;
+    private int duracao;
+    private String textoOriginal;
+    private int pFalaVendedor;
+
+    public Reuniao(){}
+
+    public Reuniao(LocalDateTime data, int duracao, String textoOriginal, int pFalaVendedor) {
+        this.data = data;
+        this.duracao = duracao;
+        this.textoOriginal = textoOriginal;
+        this.pFalaVendedor = pFalaVendedor;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getTextoOriginal() {
+        return textoOriginal;
+    }
+
+    public void setTextoOriginal(String textoOriginal) {
+        this.textoOriginal = textoOriginal;
+    }
+
+    public int getpFalaVendedor() {
+        return pFalaVendedor;
+    }
+
+    public void setpFalaVendedor(int pFalaVendedor) {
+        this.pFalaVendedor = pFalaVendedor;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=== REUNIAO ===" +
+                "\nData: " + data +
+                "\nDuracao: " + duracao + " min" +
+                "\nPercentual Fala Vendedor: " + pFalaVendedor + "%" +
+                "\nTexto Original: " + textoOriginal;
+    }
 }
