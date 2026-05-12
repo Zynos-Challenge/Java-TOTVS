ae14420b2c617ba02edff3cd9303c71ca6876a2b
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-12 19:35:09 -0300

    feat: adiciona classe Historico com List de Analise

--- src/br/com/totvs/conversacional/entities/Historico.java
+++ src/br/com/totvs/conversacional/entities/Historico.java
@@ -1,4 +1,53 @@
 package br.com.totvs.conversacional.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Historico {
+
+    private String nomeCliente;
+    private int totalReunioes;
+    private List<Analise> analises;
+
+    public Historico() {
+        this.analises = new ArrayList<>();
+    }
+
+    public Historico(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+        this.totalReunioes = 0;
+        this.analises = new ArrayList<>();
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public int getTotalReunioes() {
+        return totalReunioes;
+    }
+
+    public void setTotalReunioes(int totalReunioes) {
+        this.totalReunioes = totalReunioes;
+    }
+
+    public List<Analise> getAnalises() {
+        return analises;
+    }
+
+    public void setAnalises(List<Analise> analises) {
+        this.analises = analises;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=== HISTORICO ===" +
+                "\nCliente: " + nomeCliente +
+                "\nTotal de Reunioes: " + totalReunioes +
+                "\nAnalises: " + analises;
+    }
 }
