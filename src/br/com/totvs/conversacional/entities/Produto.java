1e2dd0955cbd1b2e0560f43a318cfcb615d83ea7
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-12 18:39:33 -0300

    feat: adiciona classe Produto

--- src/br/com/totvs/conversacional/entities/Produto.java
+++ src/br/com/totvs/conversacional/entities/Produto.java
@@ -1,4 +1,59 @@
 package br.com.totvs.conversacional.entities;
 
 public class Produto {
+
+    public String nome;
+    public String categoria;
+    public String status;
+    public String versaoMencao;
+
+    public Produto(){}
+
+    public Produto(String nome, String categoria, String status, String versaoMencao) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.status = status;
+        this.versaoMencao = versaoMencao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getVersaoMencao() {
+        return versaoMencao;
+    }
+
+    public void setVersaoMencao(String versaoMencao) {
+        this.versaoMencao = versaoMencao;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=== PRODUTO ===" +
+                "\nNome: " + nome +
+                "\nCategoria: " + categoria +
+                "\nStatus: " + status +
+                "\nVersao Mencao: " + versaoMencao;
+    }
 }
