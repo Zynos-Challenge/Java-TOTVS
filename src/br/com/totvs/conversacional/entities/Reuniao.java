e38aa0d809a06801fd875b99221bf868e72d82ee
Hyann <contato.hyann@gmail.com>
2026-05-21 18:42:01 -0300

    feat(entities): adicionar campos extras do JSON na classe Reuniao

--- src/br/com/totvs/conversacional/entities/Reuniao.java
+++ src/br/com/totvs/conversacional/entities/Reuniao.java
@@ -9,46 +9,54 @@ public class Reuniao {
     private String textoOriginal;
     private int pFalaVendedor;
 
+    //Campos extras do JSON
+    private String segmento;
+    private int notaNps;
+    private String tipoRecurso;
+    private String uf;
+    private String faixaFaturamento;
+
+
     public Reuniao(){}
 
-    public Reuniao(LocalDateTime data, int duracao, String textoOriginal, int pFalaVendedor) {
+    public Reuniao(LocalDateTime data, int duracao, String textoOriginal, int pFalaVendedor, String segmento, int notaNps, String tipoRecurso, String uf, String faixaFaturamento) {
         this.data = data;
         this.duracao = duracao;
         this.textoOriginal = textoOriginal;
         this.pFalaVendedor = pFalaVendedor;
+        this.segmento = segmento;
+        this.notaNps = notaNps;
+        this.tipoRecurso = tipoRecurso;
+        this.uf = uf;
+        this.faixaFaturamento = faixaFaturamento;
     }
 
-    public LocalDateTime getData() {
-        return data;
-    }
+    public LocalDateTime getData() { return data; }
+    public void setData(LocalDateTime data) { this.data = data; }
 
-    public void setData(LocalDateTime data) {
-        this.data = data;
-    }
+    public int getDuracao() { return duracao; }
+    public void setDuracao(int duracao) { this.duracao = duracao; }
 
-    public int getDuracao() {
-        return duracao;
-    }
+    public String getTextoOriginal() { return textoOriginal; }
+    public void setTextoOriginal(String textoOriginal) { this.textoOriginal = textoOriginal; }
 
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
-    }
+    public int getPFalaVendedor() { return pFalaVendedor; }
+    public void setPFalaVendedor(int pFalaVendedor) { this.pFalaVendedor = pFalaVendedor; }
 
-    public String getTextoOriginal() {
-        return textoOriginal;
-    }
+    public String getSegmento() { return segmento; }
+    public void setSegmento(String segmento) { this.segmento = segmento; }
 
-    public void setTextoOriginal(String textoOriginal) {
-        this.textoOriginal = textoOriginal;
-    }
+    public int getNotaNps() { return notaNps; }
+    public void setNotaNps(int notaNps) { this.notaNps = notaNps; }
 
-    public int getpFalaVendedor() {
-        return pFalaVendedor;
-    }
+    public String getTipoRecurso() { return tipoRecurso; }
+    public void setTipoRecurso(String tipoRecurso) { this.tipoRecurso = tipoRecurso; }
 
-    public void setpFalaVendedor(int pFalaVendedor) {
-        this.pFalaVendedor = pFalaVendedor;
-    }
+    public String getUf() { return uf; }
+    public void setUf(String uf) { this.uf = uf; }
+
+    public String getFaixaFaturamento() { return faixaFaturamento; }
+    public void setFaixaFaturamento(String faixaFaturamento) { this.faixaFaturamento = faixaFaturamento; }
 
     @Override
     public String toString() {
@@ -56,6 +64,11 @@ public class Reuniao {
                 "\nData: " + data +
                 "\nDuracao: " + duracao + " min" +
                 "\nPercentual Fala Vendedor: " + pFalaVendedor + "%" +
+                "\nSegmento: " + segmento +
+                "\nNPS: " + notaNps +
+                "\nTipo Recurso: " + tipoRecurso +
+                "\nUF: " + uf +
+                "\nFaixa Faturamento: " + faixaFaturamento +
                 "\nTexto Original: " + textoOriginal;
     }
 }
